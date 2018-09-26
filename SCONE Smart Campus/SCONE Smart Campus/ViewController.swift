abc44a80888fbe70ee99025d2890e72eb8c1d900
JihoChoi <jihochoi1993@gmail.com>
2018-09-26 13:59:08 +0900

    Init the project

---
+++ SCONE Smart Campus/SCONE Smart Campus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SCONE Smart Campus
+//
+//  Created by Jiho Choi on 2018. 9. 26..
+//  Copyright © 2018년 iosteam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
